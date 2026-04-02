2c33867a80be27ef44b99a5c03dfb8bda63ad4ba
Sidharth Nair <sidharth.nair@brine.fi>
2026-04-03 00:36:43 +0530

    Formatting

--- script/AcceptDefaultAdminTransfer.s.sol
+++ script/AcceptDefaultAdminTransfer.s.sol
@@ -6,14 +6,12 @@ import {
     AccessControlDefaultAdminRulesUpgradeable
 } from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
 
-
 contract AcceptDefaultAdminTransfer is Script {
     function run() external {
         address target = 0x8Fcd23142047A3073ed332a0Ed07d1e8D2BD5177;
 
-        bytes memory data = abi.encodeWithSelector(
-            AccessControlDefaultAdminRulesUpgradeable.acceptDefaultAdminTransfer.selector
-        );
+        bytes memory data =
+            abi.encodeWithSelector(AccessControlDefaultAdminRulesUpgradeable.acceptDefaultAdminTransfer.selector);
 
         console.log("Target:", target);
         console.log("Calldata:");
