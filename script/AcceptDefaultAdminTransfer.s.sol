0489df089e76d84a1c6ed3ae274d9a1a7e342b0a
Sidharth Nair <sidharth.nair@brine.fi>
2026-04-03 01:02:55 +0530

    Fixed formatting

--- script/AcceptDefaultAdminTransfer.s.sol
+++ script/AcceptDefaultAdminTransfer.s.sol
@@ -19,5 +19,4 @@ contract AcceptDefaultAdminTransfer is Script {
     }
 }
 
-
 //forge script ./script/AcceptDefaultAdminTransfer.s.sol:AcceptDefaultAdminTransfer --rpc-url eth_mainnet --account multipli-deployer --sender 0x8cFee31bf3A57EC2C86D9e0f476Bd36aCA611Fa5 --broadcast -vvvv
